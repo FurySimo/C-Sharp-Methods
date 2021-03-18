072924ebf0c0acd79939afbdeefb88fd766b3663
Z M Asif Jamil <80680790+FurySimo@users.noreply.github.com>
2021-03-18 15:31:37 +0600

    Add files via upload

---
+++ C Sharp Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace C_Sharp_Methods
+{
+    class Program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("I just got executed.");
+        }
+        static void Main(string[] args)
+        {
+           
+            {
+                MyMethod();
+                MyMethod();
+                MyMethod();
+            }
+        }
+    }
+}
